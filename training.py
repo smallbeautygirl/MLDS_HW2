25fbf5166f578116f63bde87180a9da1dfbaa085
HuiHsuanWang <ecindy17@yahoo.com.tw>
2022-11-16 17:57:37 +0800

    新增範例程式

---
+++ training.py
@@ -0,0 +1,119 @@
+import numpy as np
+from numpy import nan
+import pandas as pd
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import MinMaxScaler
+
+
+def fill_missing(data):
+    one_day = 24*60
+    for row in range(data.shape[0]):
+        for col in range(data.shape[1]):
+            if np.isnan(data[row, col]):
+                data[row, col] = data[row-one_day, col]
+
+def evaluate_model(y_true, y_predicted):
+    scores = []
+
+    # calculate scores for each day
+    for i in range(y_true.shape[1]):
+        mse = mean_squared_error(y_true[:, i], y_predicted[:, i])
+        rmse = np.sqrt(mse)
+        scores.append(rmse)
+
+    # calculate score for whole prediction
+    total_score = 0
+    for row in range(y_true.shape[0]):
+        for col in range(y_predicted.shape[1]):
+            total_score = total_score + (y_true[row, col] - y_predicted[row, col])**2
+    total_score = np.sqrt(total_score//(y_true.shape[0]*y_predicted.shape[1]))
+
+    return total_score, scores
+data = pd.read_csv('household_power_consumption.txt', sep=';', parse_dates=True, low_memory=False)
+
+print(data.head())
+data['date_time'] = data['Date'].str.cat(data['Time'], sep=' ')
+data.drop(['Date', 'Time'], inplace=True, axis=1)
+data.set_index(['date_time'], inplace=True)
+data.replace('?', nan, inplace=True)
+data = data.astype('float')
+
+# print(data.info())
+# print(np.isnan(data).sum())
+
+fill_missing(data.values)
+# print(np.isnan(data).sum())
+# print(data.info())
+# print(data.shape)
+# print(data.head)
+
+
+data.to_csv('cleaned_data.csv')
+dataset = pd.read_csv('cleaned_data.csv', parse_dates=True,
+                      index_col='date_time', low_memory=False)
+data = dataset.resample('D').sum()
+
+data_train = data.loc[:'2009-12-31', :]['Global_active_power']
+data_test = data['2010']['Global_active_power']
+
+data_train = np.array(data_train)
+
+data_test = np.array(data_test)
+X_train, y_train = [], []
+X_test, y_test = [], []
+
+for i in range(7, len(data_train) - 7):
+    X_train.append(data_train[i - 7:i])
+    y_train.append(data_train[i:i + 7])
+
+    
+for i in range(7, len(data_test) - 7):
+    X_test.append(data_test[i - 7:i])
+    y_test.append(data_test[i:i + 7])
+
+
+X_train, y_train = np.array(X_train), np.array(y_train)
+X_test, y_test = np.array(X_test), np.array(y_test)
+
+print(X_train.shape, y_train.shape)
+print(X_test.shape, y_test.shape)
+
+x_scaler = MinMaxScaler()
+X_train = x_scaler.fit_transform(X_train)
+
+y_scaler = MinMaxScaler()
+y_train = y_scaler.fit_transform(y_train)
+
+X_test = x_scaler.transform(X_test)
+y_test = y_scaler.transform(y_test)
+
+# print(pd.DataFrame(X_train).head())
+
+X_train = X_train.reshape(1098, 7, 1)
+
+reg = Sequential()
+reg.add(LSTM(units=200, activation='relu', input_shape=(7, 1)))
+reg.add(Dense(7))
+
+reg.compile(loss='mse', optimizer='adam')
+reg.fit(X_train, y_train, epochs=100)
+
+
+# # 將模型儲存至 HDF5 檔案中
+reg.save('my_model.h5')  # creates a HDF5 file 'my_model.h5'
+
+X_test = X_test.reshape(331, 7, 1)
+
+y_pred = reg.predict(X_test)
+
+y_pred = y_scaler.inverse_transform(y_pred)
+print(y_pred)
+print('==================')
+y_true = y_scaler.inverse_transform(y_test)
+print(y_true)
+print('==================')
+evaluate_model(y_true, y_pred)
+
+print(np.std(y_true[0]))
