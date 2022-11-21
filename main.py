5a08d50109122249e08409b7ddbbd07e14cd76e5
vivian <vivian.sh.fan@dct-cloud.com>
2022-11-21 22:00:03 +0800

    feat: :sparkles: update training/train.py & main.py (寫成function

--- main.py
+++ main.py
@@ -51,9 +51,9 @@ def evaluate_model(last_data, y_true_c, y_predicted_c, y_true_g, y_predicted_g):
         rmse_g = np.sqrt(mse_g)
         # scores.append(rmse_c)
         action
-        if rmse_g > rmse_c:
+        if rmse_g > rmse_c or (i >= 5 and i <= 15):
             action = "sell"
-            price = random.uniform(1.15, 2.7)
+            price = random.uniform(0.7, 2.7)
         else:
             action = "buy"
             price = random.uniform(
@@ -77,32 +77,18 @@ def evaluate_model(last_data, y_true_c, y_predicted_c, y_true_g, y_predicted_g):
     # return total_score, scores
 
 
-if __name__ == "__main__":
-    args = config()
-
-    df = pd.read_csv('./training/cleaned_data.csv', parse_dates=True)
-    df = df.set_index(['time'])
-
-    data_train = df['consumption']
-    data_train = np.array(data_train)
-
-    data_train2 = df['generation']
-    data_train2 = np.array(data_train2)
+def training_consumption(df):
+    data_train_c = df['consumption']
+    data_train_c = np.array(data_train_c)
 
     x_train, y_train = [], []
-    x_train2, y_train2 = [], []
 
-    # 24 hours
-    for i in range(24, len(data_train) - 24):
-        x_train.append(data_train[i - 24: i])
-        y_train.append(data_train[i: i + 24])
-
-    for i in range(24, len(data_train2) - 24):
-        x_train2.append(data_train2[i - 24: i])
-        y_train2.append(data_train2[i: i + 24])
+    # 24*7 = 168 hours
+    for i in range(24, len(data_train_c) - 24):
+        x_train.append(data_train_c[i - 24: i])
+        y_train.append(data_train_c[i: i + 24])
 
     x_train, y_train = np.array(x_train), np.array(y_train)
-    x_train2, y_train2 = np.array(x_train2), np.array(y_train2)
 
     # Normalize dataset between 0 and 1 with MinMaxScaler
     x_scaler = MinMaxScaler()
@@ -113,45 +99,55 @@ if __name__ == "__main__":
 
     x_train = x_train.reshape(x_train.shape[0], 24, 1)
 
-    ###
-    x_scaler2 = MinMaxScaler()
-    x_train2 = x_scaler2.fit_transform(x_train2)
+    # training
+    # reg = Sequential()
+    # reg.add(LSTM(units=200, activation='relu', input_shape=(24, 1)))
+    # reg.add(Dense(24))
 
-    y_scaler2 = MinMaxScaler()
-    y_train2 = y_scaler2.fit_transform(y_train2)
+    # reg.compile(loss='mse', optimizer='adam')
 
-    x_train2 = x_train2.reshape(x_train2.shape[0], 24, 1)
+    # reg.fit(x_train, y_train, epochs=1)
 
-# reg = Sequential()
-# reg.add(LSTM(units=200, activation='relu', input_shape=(24, 1)))
-# reg.add(Dense(24))
+    # reg.save('consumption_model.h5')
+    return x_scaler, y_scaler
 
-# reg.compile(loss='mse', optimizer='adam')
 
-# reg.fit(x_train, y_train, epochs=1)
+def training_generation(df):
+    data_train_g = df['generation']
+    data_train_g = np.array(data_train_g)
 
-# reg.save('consumption_model.h5')
+    x_train, y_train = [], []
 
-    consumption_data = pd.read_csv(
-        args.consumption, parse_dates=True)
-    consumption_data = consumption_data.set_index(['time'])
-    consumption_data = consumption_data['consumption']
+    # 24*7 = 168 hours
+    for i in range(24, len(data_train_g) - 24):
+        x_train.append(data_train_g[i - 24: i])
+        y_train.append(data_train_g[i: i + 24])
 
-    consumption_data = np.array(consumption_data)
+    x_train, y_train = np.array(x_train), np.array(y_train)
 
-    generation_data = pd.read_csv(
-        args.generation, parse_dates=True)
+    # Normalize dataset between 0 and 1 with MinMaxScaler
+    x_scaler = MinMaxScaler()
+    x_train = x_scaler.fit_transform(x_train)
 
-    print(generation_data.iloc[-1].tolist())
-    last_data = generation_data.iloc[-1].tolist()[0]
-    print(last_data.split(' ')[0])
-    last_data = datetime.strptime(last_data.split(' ')[0], "%Y-%m-%d")
-    print(last_data)
-    generation_data = generation_data.set_index(['time'])
-    generation_data = generation_data['generation']
+    y_scaler = MinMaxScaler()
+    y_train = y_scaler.fit_transform(y_train)
 
-    generation_data = np.array(generation_data)
+    x_train = x_train.reshape(x_train.shape[0], 24, 1)
 
+    # training
+    # reg2 = Sequential()
+    # reg2.add(LSTM(units=200, activation='relu', input_shape=(24, 1)))
+    # reg2.add(Dense(24))
+
+    # reg2.compile(loss='mse', optimizer='adam')
+
+    # reg2.fit(x_train, y_train, epochs=1)
+
+    # reg2.save('generation_model.h5')
+    return x_scaler, y_scaler
+
+
+def test_consumption(consumption_data, x_scaler, y_scaler):
     x_test_c, y_test_c = [], []
 
     # Split test data by day (24 hours)
@@ -166,6 +162,15 @@ if __name__ == "__main__":
 
     x_test_c = x_test_c.reshape(x_test_c.shape[0], 24, 1)
 
+    consumption_model = tf.keras.models.load_model('consumption_model.h5')
+    y_pred_c = consumption_model.predict(x_test_c)
+    y_pred_c = y_scaler.inverse_transform(y_pred_c)
+    y_true_c = y_scaler.inverse_transform(y_test_c)
+
+    return y_pred_c, y_true_c
+
+
+def test_generation(generation_data, x_scaler, y_scaler):
     x_test_g, y_test_g = [], []
     # Split test data by day (24 hours)
     for i in range(24, len(generation_data)-24):
@@ -174,23 +179,54 @@ if __name__ == "__main__":
 
     x_test_g, y_test_g = np.array(x_test_g), np.array(y_test_g)
 
-    x_test_g = x_scaler2.transform(x_test_g)
-    y_test_g = y_scaler2.transform(y_test_g)
+    x_test_g = x_scaler.transform(x_test_g)
+    y_test_g = y_scaler.transform(y_test_g)
 
     x_test_g = x_test_g.reshape(x_test_g.shape[0], 24, 1)
 
-    consumption_model = tf.keras.models.load_model('consumption_model.h5')
     generation_model = tf.keras.models.load_model('generation_model.h5')
-    # Show the model architecture
-    # print(consumption_model.summary())
-    y_pred_c = consumption_model.predict(x_test_c)
-    y_pred_g = consumption_model.predict(x_test_g)
 
-    y_pred_c = y_scaler.inverse_transform(y_pred_c)
+    y_pred_g = generation_model.predict(x_test_g)
     y_pred_g = y_scaler.inverse_transform(y_pred_g)
-
-    y_true_c = y_scaler.inverse_transform(y_test_c)
     y_true_g = y_scaler.inverse_transform(y_pred_g)
+
+    return y_pred_g, y_true_g
+
+
+if __name__ == "__main__":
+    args = config()
+
+    df = pd.read_csv('./training/cleaned_data.csv', parse_dates=True)
+    df = df.set_index(['time'])
+
+    x_scaler_c, y_scaler_c = training_consumption(df)
+    x_scaler_g, y_scaler_g = training_generation(df)
+
+    # consumption
+    consumption_data = pd.read_csv(
+        args.consumption, parse_dates=True)
+    consumption_data = consumption_data.set_index(['time'])
+    consumption_data = consumption_data['consumption']
+    consumption_data = np.array(consumption_data)
+
+    # generation
+    generation_data = pd.read_csv(
+        args.generation, parse_dates=True)
+
+    print(generation_data.iloc[-1].tolist())
+    last_data = generation_data.iloc[-1].tolist()[0]
+    print(last_data.split(' ')[0])
+    last_data = datetime.strptime(last_data.split(' ')[0], "%Y-%m-%d")
+    print(last_data)
+    generation_data = generation_data.set_index(['time'])
+    generation_data = generation_data['generation']
+
+    generation_data = np.array(generation_data)
+
+    y_pred_c, y_true_c = test_consumption(
+        consumption_data, x_scaler_c, y_scaler_c)
+    y_pred_g, y_true_g = test_generation(
+        generation_data, x_scaler_g, y_scaler_g)
 
     result = evaluate_model(last_data, y_true_c, y_pred_c, y_true_g, y_pred_g)
     print(result)
--- training/train.py
+++ training/train.py
@@ -12,91 +12,99 @@ import glob
 import tensorflow as tf
 
 
-# training_data_dir = './training/training_data'
+def training_consumption(df):
+    data_train_c = df['consumption']
+    data_train_c = np.array(data_train_c)
 
-# all_files = glob.glob(os.path.join(training_data_dir, "*.csv"))
+    x_train, y_train = [], []
 
-# df = pd.concat((pd.read_csv(f, parse_dates=True)
-#                for f in all_files), ignore_index=True)
-# df.set_index(['time'])
+    # 24*7 = 168 hours
+    for i in range(24, len(data_train_c) - 24):
+        x_train.append(data_train_c[i - 24: i])
+        y_train.append(data_train_c[i: i + 24])
 
-# print(df.head())
-# print(df.tail())
-# print(type(df))
-# # columns=['time','generation','consumption']
-# # for csv_file in os.listdir(training_data_dir):
-# #     data = pd.read_csv(os.path.join(training_data_dir, csv_file),
-# #                        sep=';', parse_dates=True, low_memory=False)
-# print(df.describe())
-# print(123)
-# print(df.head())
-# print(df.info())
-# print(df.shape)
+    x_train, y_train = np.array(x_train), np.array(y_train)
 
-# df.to_csv('./training/cleaned_data.csv')
-df = pd.read_csv('./training/cleaned_data.csv', parse_dates=True)
-df = df.set_index(['time'])
-print(df.head())
-print(df['consumption'])
-data_train = df['consumption']
-print(data_train)
-data_train = np.array(data_train)
+    # Normalize dataset between 0 and 1 with MinMaxScaler
+    x_scaler = MinMaxScaler()
+    x_train = x_scaler.fit_transform(x_train)
 
-data_train2 = df['generation']
-data_train2 = np.array(data_train2)
+    y_scaler = MinMaxScaler()
+    y_train = y_scaler.fit_transform(y_train)
 
-x_train, y_train = [], []
-x_train2, y_train2 = [], []
+    x_train = x_train.reshape(x_train.shape[0], 24, 1)
 
-# 24*7 = 168 hours
-for i in range(24, len(data_train) - 24):
-    x_train.append(data_train[i - 24: i])
-    y_train.append(data_train[i: i + 24])
+    # training
+    reg = Sequential()
+    reg.add(LSTM(units=200, activation='relu', input_shape=(24, 1)))
+    reg.add(Dense(24))
 
-for i in range(24, len(data_train2) - 24):
-    x_train2.append(data_train2[i - 24: i])
-    y_train2.append(data_train2[i: i + 24])
+    reg.compile(loss='mse', optimizer='adam')
 
-x_train, y_train = np.array(x_train), np.array(y_train)
-x_train2, y_train2 = np.array(x_train2), np.array(y_train2)
-print(x_train.shape, y_train.shape)
-print(x_train2.shape, y_train2.shape)
+    reg.fit(x_train, y_train, epochs=1)
 
-# Normalize dataset between 0 and 1 with MinMaxScaler
-x_scaler = MinMaxScaler()
-x_train = x_scaler.fit_transform(x_train)
-
-y_scaler = MinMaxScaler()
-y_train = y_scaler.fit_transform(y_train)
-
-x_train = x_train.reshape(x_train.shape[0], 24, 1)
-
-###
-x_scaler2 = MinMaxScaler()
-x_train2 = x_scaler2.fit_transform(x_train2)
-
-y_scaler2 = MinMaxScaler()
-y_train2 = y_scaler2.fit_transform(y_train2)
-
-x_train2 = x_train2.reshape(x_train2.shape[0], 24, 1)
-
-reg = Sequential()
-reg.add(LSTM(units=200, activation='relu', input_shape=(24, 1)))
-reg.add(Dense(24))
-
-reg.compile(loss='mse', optimizer='adam')
-
-reg.fit(x_train, y_train, epochs=1)
-
-reg.save('consumption_model.h5')
+    reg.save('consumption_model.h5')
 
 
-reg2 = Sequential()
-reg2.add(LSTM(units=200, activation='relu', input_shape=(24, 1)))
-reg2.add(Dense(24))
+def training_generation(df):
+    data_train_g = df['generation']
+    data_train_g = np.array(data_train_g)
 
-reg2.compile(loss='mse', optimizer='adam')
+    x_train, y_train = [], []
 
-reg2.fit(x_train2, y_train2, epochs=1)
+    # 24*7 = 168 hours
+    for i in range(24, len(data_train_g) - 24):
+        x_train.append(data_train_g[i - 24: i])
+        y_train.append(data_train_g[i: i + 24])
 
-reg2.save('generation_model.h5')
+    x_train, y_train = np.array(x_train), np.array(y_train)
+
+    # Normalize dataset between 0 and 1 with MinMaxScaler
+    x_scaler = MinMaxScaler()
+    x_train = x_scaler.fit_transform(x_train)
+
+    y_scaler = MinMaxScaler()
+    y_train = y_scaler.fit_transform(y_train)
+
+    x_train = x_train.reshape(x_train.shape[0], 24, 1)
+
+    # training
+    reg2 = Sequential()
+    reg2.add(LSTM(units=200, activation='relu', input_shape=(24, 1)))
+    reg2.add(Dense(24))
+
+    reg2.compile(loss='mse', optimizer='adam')
+
+    reg2.fit(x_train, y_train, epochs=1)
+
+    reg2.save('generation_model.h5')
+
+
+if __name__ == "__main__":
+    # training_data_dir = './training/training_data'
+
+    # all_files = glob.glob(os.path.join(training_data_dir, "*.csv"))
+
+    # df = pd.concat((pd.read_csv(f, parse_dates=True)
+    #                for f in all_files), ignore_index=True)
+    # df.set_index(['time'])
+
+    # print(df.head())
+    # print(df.tail())
+    # print(type(df))
+    # # columns=['time','generation','consumption']
+    # # for csv_file in os.listdir(training_data_dir):
+    # #     data = pd.read_csv(os.path.join(training_data_dir, csv_file),
+    # #                        sep=';', parse_dates=True, low_memory=False)
+    # print(df.describe())
+    # print(123)
+    # print(df.head())
+    # print(df.info())
+    # print(df.shape)
+
+    # df.to_csv('./training/cleaned_data.csv')
+    df = pd.read_csv('./training/cleaned_data.csv', parse_dates=True)
+    df = df.set_index(['time'])
+
+    training_consumption(df)
+    training_generation(df)
